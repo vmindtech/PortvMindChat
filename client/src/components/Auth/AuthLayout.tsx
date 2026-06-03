7c13415eac7f01e5ed93c6af6bacf262b331af9f
kkaptanoglu <kubilay@kaptanoglu.me>
2026-06-03 17:12:48 +0300

    change logo design

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -60,10 +60,10 @@ function AuthLayout({
     <div className="relative flex min-h-screen flex-col bg-white dark:bg-gray-900">
       <Banner />
       <BlinkAnimation active={isFetching}>
-        <div className="mt-6 h-10 w-full bg-cover">
+        <div className="mt-8 flex justify-center px-4">
           <img
             src="assets/logo.svg"
-            className="h-full w-full object-contain"
+            className="h-16 w-auto max-w-[280px] object-contain invert dark:invert-0 sm:h-20 md:h-24"
             alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? Constants.DEFAULT_APP_TITLE })}
           />
         </div>
